c1f86a53f48fa24f4947cae2faf5c9cf657dd269
Kekbaber <Kokmes45@gmail.com>
2026-08-14 15:00:40 +0300

    fix(gateway): убран ConstraintViolationException из обработчика ошибок

--- gateway/src/main/java/ru/practicum/shareit/exception/handler/GlobalExceptionHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.exception.handler;
 
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -14,7 +13,7 @@ import java.util.Map;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidation(Exception ex) {
         log.warn("Validation error: {}", ex.getMessage());
